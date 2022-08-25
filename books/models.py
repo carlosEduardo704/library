f39500b2559d538e5fb912e24f412c3e26558a29
carlosEduardo704 <carlos704eduardo@gmail.com>
2022-08-25 00:30:45 -0300

    Configurando a API para armazenar imagens

--- books/models.py
+++ books/models.py
@@ -4,6 +4,10 @@ from uuid import uuid4
 # Create your models here.
 
 
+def upload_image_book(instace, filename):
+    return f"{instace.id_book}-{filename}"
+
+
 class Books(models.Model):
     id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=255)
@@ -13,4 +17,5 @@ class Books(models.Model):
     page = models.IntegerField()
     publishing_company = models.CharField(max_length=255)
     create_at = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to=upload_image_book, blank=True, null=True)
 
--- library/urls.py
+++ library/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from rest_framework import routers
 from books.api import viewset as bookviewsets
 
@@ -26,4 +29,4 @@ route.register(r'books', bookviewsets.BookViewSet, basename='Books')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
